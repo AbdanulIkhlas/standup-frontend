502eaaeb2f9e354b0810fb1b0fcb56a0abc4e53b
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-05-27 19:53:09 +0700

    upload project

---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="bg-blue-400">
+      <h1 className="text-5xl font-bold underline text-center">Hello world!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
