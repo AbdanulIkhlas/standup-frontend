95c727692537a61b032894a79273b2a5f84b8344
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-06-03 03:54:40 +0700

    konfigurasi BE Register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,48 @@
 import InputForm from "../components/elements/forms/InputForm";
 import SubmitButton from "../components/elements/buttons/SubmitButton";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [verifyPassword, setVerifyPassword] = useState("");
+  const navigate = useNavigate(); // Updated hook
+  const [message, setMessage] = useState("");
+
+  const register = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        "https://standup-backend-g64dafi2la-et.a.run.app/register",
+        {
+          email: email,
+          password: password,
+          nama: name,
+          alamat: address,
+          username: username,
+          notelp: phone,
+        },
+        { withCredentials: false } // Pastikan ini diset ke false
+      );
+      navigate("/login");
+      // jika berhasil regist
+      if (e.response) {
+        setMessage(e.response.data.message);
+      }
+    } catch (error) {
+      // jika error
+      if (error.response) {
+        setMessage(error.response.data.message);
+      }
+    }
+  };
+
   return (
     <div className="font-roboto relative">
       <img
@@ -15,13 +55,14 @@ const Register = () => {
       <main className="w-full absolute z-30 px-20 h-screen flex flex-col items-center justify-center">
         {/* FORM REGIST */}
         <form
-          action=""
+          onSubmit={register}
           className="py-14 px-12 text-white flex flex-col justify-center items-center border border-white bg-[#612125] bg-opacity-80 backdrop-blur-md rounded-lg"
         >
           <h1 className="text-2xl font-bold mb-12">DAFTAR</h1>
           {/* Data */}
           <div className="flex gap-16">
             <div>
+              <p>{message}</p>
               {/* nama */}
               <div className="mb-4">
                 <InputForm
@@ -29,6 +70,8 @@ const Register = () => {
                   type="text"
                   name="nama"
                   id="nama"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
                   placeholder="masukkan nama anda"
                   customClass="mb-3"
                 />
@@ -40,6 +83,8 @@ const Register = () => {
                   type="text"
                   name="alamat"
                   id="alamat"
+                  value={address}
+                  onChange={(event) => setAddress(event.target.value)}
                   placeholder="ex: Jl. Raya Cibadak"
                   customClass="mb-3"
                 />
@@ -51,6 +96,8 @@ const Register = () => {
                   type="telp"
                   name="notelp"
                   id="notelp"
+                  value={phone}
+                  onChange={(event) => setPhone(event.target.value)}
                   placeholder="ex: 08xxxxxxxxxx"
                   customClass="mb-3"
                 />
@@ -65,6 +112,8 @@ const Register = () => {
                   type="text"
                   name="username"
                   id="username"
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
                   placeholder="masukkan username"
                   customClass="mb-3"
                 />
@@ -76,6 +125,8 @@ const Register = () => {
                   type="email"
                   name="email"
                   id="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
                   placeholder="masukkan email anda"
                   customClass="mb-3"
                 />
@@ -87,6 +138,8 @@ const Register = () => {
                   type="password"
                   name="password"
                   id="password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
                   placeholder="masukkan password anda"
                   customClass="mb-3"
                 />
@@ -98,6 +151,8 @@ const Register = () => {
                   type="password"
                   name="verifyPassword"
                   id="verifyPassword"
+                  value={verifyPassword}
+                  onChange={(event) => setVerifyPassword(event.target.value)}
                   placeholder="verifikasi password anda"
                   customClass="mb-3"
                 />
